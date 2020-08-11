8797ae11e52be584b5e3ef560090360e50a06a7a
ぴいた <pitolick@gmail.com>
2020-08-11 19:21:19 +0900

    header style

--- header.php
+++ header.php
@@ -10,27 +10,32 @@
 <body <?php body_class(); ?>>
 	<div id="wrapper" class="hfeed">
 		<header id="header">
-			<div id="branding">
-				<div id="site-title">
-					<?php if (is_front_page() || is_home() || is_front_page() && is_home()) {
-							echo '<h1>';
-					} ?>
-						<?php if(has_custom_logo()) : ?>
-							<?php the_custom_logo(); ?>
-						<?php else: ?>
-							<a href="<?php echo esc_url(home_url('/')); ?>"
-								title="<?php echo esc_html(get_bloginfo('name')); ?>"
-								rel="home"><?php echo esc_html(get_bloginfo('name')); ?></a>
-						<?php endif; ?>
-					<?php if (is_front_page() || is_home() || is_front_page() && is_home()) {
-							echo '</h1>';
-					} ?>
+			<div class="header-wrap container">
+				<div id="branding">
+					<div id="site-title">
+						<?php if (is_front_page() || is_home() || is_front_page() && is_home()) {
+								echo '<h1>';
+						} ?>
+							<?php if(has_custom_logo()) : ?>
+								<?php the_custom_logo(); ?>
+							<?php else: ?>
+								<a href="<?php echo esc_url(home_url('/')); ?>"
+									title="<?php echo esc_html(get_bloginfo('name')); ?>"
+									rel="home"><?php echo esc_html(get_bloginfo('name')); ?></a>
+							<?php endif; ?>
+						<?php if (is_front_page() || is_home() || is_front_page() && is_home()) {
+								echo '</h1>';
+						} ?>
+					</div>
 				</div>
+				<!-- <div id="search"><?php get_search_form(); ?>
+				</div> -->
+				<input id="nav-input" type="checkbox" class="nav-unshown" checked>
+				<label id="nav-open" for="nav-input" class="nav-unshown"><i class="fas fa-bars fa-lg"></i></label>
+				<label class="nav-unshown" id="nav-close" for="nav-input"></label>
+				<nav id="menu">
+					<?php wp_nav_menu(array( 'theme_location' => 'main-menu' )); ?>
+				</nav>
 			</div>
-			<nav id="menu">
-				<div id="search"><?php get_search_form(); ?>
-				</div>
-				<?php wp_nav_menu(array( 'theme_location' => 'main-menu' )); ?>
-			</nav>
 		</header>
-		<div id="container">
+		<div id="container" class="container">
