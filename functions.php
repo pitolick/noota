bd5137683446031c671c28f7b7e108f2e9e6fac8
ぴいた <pitolick@gmail.com>
2020-08-13 09:44:27 +0900

    add function Bloginfo Shortcode

--- functions.php
+++ functions.php
@@ -111,3 +111,30 @@ function catch_thumbnail_image($get_size = 'thumbnail', $altimg_id = null) {
 	}
 	return $image;
 }
+
+/**
+ * bloginfo()をショートコード化
+ * Plugin Name: Bloginfo Shortcode
+ * Description: Allows bloginfo() as a shortcode.
+ * Author: Giuseppe Mazzapica
+ * Author URI: http://gm.zoomlab.it
+ * License: MIT
+ */
+add_shortcode('bloginfo', function($atts) {
+
+	$atts = shortcode_atts(array('filter'=>'', 'info'=>''), $atts, 'bloginfo');
+
+	$infos = array(
+		'name', 'description',
+		'wpurl', 'url', 'pingback_url',
+		'admin_email', 'charset', 'version', 'html_type', 'language',
+		'atom_url', 'rdf_url','rss_url', 'rss2_url',
+		'comments_atom_url', 'comments_rss2_url',
+	);
+
+	$filter = in_array(strtolower($atts['filter']), array('raw', 'display'), true)
+		? strtolower($atts['filter'])
+		: 'display';
+
+	return in_array($atts['info'], $infos, true) ? get_bloginfo($atts['info'], $filter) : '';
+});
