2bd17ff25de1754f2f0328260d3c2bdb85fa2d59
ぴいた <pitolick@gmail.com>
2020-08-30 17:42:33 +0900

    mod single thumbnail link

--- template-parts/entry-content.php
+++ template-parts/entry-content.php
@@ -18,7 +18,7 @@
 <div class="entry-content">
 	<?php if (has_post_thumbnail()) : ?>
 		<div class="entry-thumbnail">
-			<a href="<?php $src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full', false); echo esc_url($src[0]); ?>"
+			<a href="<?php the_permalink(); ?>"
 			title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail(); ?></a>
 		</div>
 	<?php endif; ?>
